bdb82c07e73b411a84b6ceb75b189042dd0bae18
joa1rocha <joa1.rocha@gmail.com>
2018-04-12 21:16:05 +0100

    updated live IP address

--- wp-config.php
+++ wp-config.php
@@ -21,7 +21,7 @@
 // Set your environment/url pairs
 $environments = array(
 	'local'       => 'localhost',
-	'production'  => '52.56.81.152'
+	'production'  => '52.56.78.193'
 );
 
 // Get the hostname
