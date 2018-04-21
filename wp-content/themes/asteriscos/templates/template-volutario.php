40df22570ac7dd431653e50a4f979cdf3f2a56fa
joa1rocha <joa1.rocha@gmail.com>
2018-04-21 22:44:19 +0100

    fixed volutarios page

--- wp-content/themes/asteriscos/templates/template-volutario.php
+++ wp-content/themes/asteriscos/templates/template-volutario.php
@@ -12,7 +12,7 @@ $imagem = get_field('imagem') ?: '';
 $log = get_field('log') ?: [];
 ?>
     <!-- Main -->
-    <div id="main" class="items">
+    <div id="main" class="voluntario">
         <!-- Featured Post -->
         <article class="post featured">
             <header class="major">
