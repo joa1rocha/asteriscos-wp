c7888d5c31b21b0bc591369d317e887ae2fd359e
joa1rocha <joa1.rocha@gmail.com>
2018-09-01 18:13:32 +0100

    added a few required fixes due to the woocommerce update

--- wp-content/themes/asteriscos/woocommerce/archive-product.php
+++ wp-content/themes/asteriscos/woocommerce/archive-product.php
@@ -10,17 +10,14 @@
  * happen. When this occurs the version of the template file will be bumped and
  * the readme will list any important changes.
  *
- * @see 	    https://docs.woocommerce.com/document/template-structure/
- * @author 		WooThemes
- * @package 	WooCommerce/Templates
- * @version     3.3.0
+ * @see https://docs.woocommerce.com/document/template-structure/
+ * @package WooCommerce/Templates
+ * @version 3.4.0
  */
 
-if ( ! defined( 'ABSPATH' ) ) {
-	exit;
-}
+defined( 'ABSPATH' ) || exit;
 
-get_header();
+get_header( 'shop' );
 
 /**
  * Hook: woocommerce_before_main_content.
@@ -32,39 +29,38 @@ get_header();
 do_action( 'woocommerce_before_main_content' );
 
 if ( have_posts() ) {
-
 	woocommerce_product_loop_start();
 
-	if ( wc_get_loop_prop( 'total' ) ) {
-		while ( have_posts() ) {
-			the_post();
+    if ( wc_get_loop_prop( 'total' ) ) {
+        while ( have_posts() ) {
+            the_post();
 
-			/**
-			 * Hook: woocommerce_shop_loop.
-			 *
-			 * @hooked WC_Structured_Data::generate_product_data() - 10
-			 */
-			do_action( 'woocommerce_shop_loop' );
+            /**
+             * Hook: woocommerce_shop_loop.
+             *
+             * @hooked WC_Structured_Data::generate_product_data() - 10
+             */
+            do_action( 'woocommerce_shop_loop' );
 
-			wc_get_template_part( 'content', 'product' );
-		}
-	}
+            wc_get_template_part( 'content', 'product' );
+        }
+    }
 
-	woocommerce_product_loop_end();
+    woocommerce_product_loop_end();
 
-	/**
-	 * Hook: woocommerce_after_shop_loop.
-	 *
-	 * @hooked woocommerce_pagination - 10
-	 */
-	do_action( 'woocommerce_after_shop_loop' );
+    /**
+     * Hook: woocommerce_after_shop_loop.
+     *
+     * @hooked woocommerce_pagination - 10
+     */
+    do_action( 'woocommerce_after_shop_loop' );
 } else {
-	/**
-	 * Hook: woocommerce_no_products_found.
-	 *
-	 * @hooked wc_no_products_found - 10
-	 */
-	do_action( 'woocommerce_no_products_found' );
+    /**
+     * Hook: woocommerce_no_products_found.
+     *
+     * @hooked wc_no_products_found - 10
+     */
+    do_action( 'woocommerce_no_products_found' );
 }
 
 /**
